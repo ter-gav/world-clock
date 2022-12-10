d08915a235f340a14c002b6b31b58ffa23516edf
ter-gav <86326060+ter-gav@users.noreply.github.com>
2022-12-10 17:05:44 +0100

    added moment.js and functions for time update

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,20 @@
+function updateTime() {
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
 
+  newYorkDateElement.innerHTML = newYorkTime.format("dddd Do, YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format("HH:mm:ss");
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("dddd Do, YYYY");
+  londonTimeElement.innerHTML = londonTime.format("HH:mm:ss");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
